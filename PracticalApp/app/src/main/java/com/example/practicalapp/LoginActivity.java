e39381c7b3c081e91922af313c27f4607ae7794c
tejaskb04 <tejaskb@gmail.com>
2017-08-22 14:10:33 -0700

    Login Request and Response, Minor Changes

--- PracticalApp/app/src/main/java/com/example/practicalapp/LoginActivity.java
+++ PracticalApp/app/src/main/java/com/example/practicalapp/LoginActivity.java
@@ -1,10 +1,19 @@
 package com.example.practicalapp;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,13 +21,48 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        Button registerBtn = (Button) findViewById(R.id.register);
+        final EditText etUsername = (EditText) findViewById(R.id.userId);
+        final EditText etPassword = (EditText) findViewById(R.id.pass);
+        final Button loginBtn = (Button) findViewById(R.id.login);
+        final Button registerBtn = (Button) findViewById(R.id.register);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                Intent intent = new Intent(LoginActivity.this, UserAreaActivity.class);
+                                LoginActivity.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+                loginRequest.setShouldCache(false);
+                RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+                requestQueue.add(loginRequest);
+            }
+        });
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
+
     }
 }
--- PracticalApp/app/src/main/java/com/example/practicalapp/LoginRequest.java
+++ PracticalApp/app/src/main/java/com/example/practicalapp/LoginRequest.java
@@ -1,20 +1,20 @@
 package com.example.practicalapp;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterRequest extends StringRequest {
-    private static final String REGISTER_REQUEST_URL = "wishlist136.000webhostapp.com/Register.php";
+public class LoginRequest extends StringRequest {
+    private static final String LOGIN_REQUEST_URL = "wishlist136.000webhostapp.com/Login.php";
     private Map<String, String> params;
 
-    public RegisterRequest(String email, String username, String password,
+    public LoginRequest(String username, String password,
                            Response.Listener<String> responseListener) {
-        super(Method.POST, REGISTER_REQUEST_URL, responseListener, null);
+        super(Request.Method.POST, LOGIN_REQUEST_URL, responseListener, null);
         params = new HashMap<>();
-        params.put("email", email);
         params.put("username", username);
         params.put("password", password);
     }
